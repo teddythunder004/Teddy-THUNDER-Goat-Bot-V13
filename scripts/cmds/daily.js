7abf6c106ca1217a04c0da74250b58b5da30344c
Shibai Otsutsuki <shibaiotsutsuki83@gmail.com>
2024-10-05 08:04:09 +0000

    Update daily.js

--- scripts/cmds/daily.js
+++ scripts/cmds/daily.js
@@ -47,7 +47,7 @@ module.exports = {
 			saturday: "Saturday",
 			sunday: "Sunday",
 			alreadyReceived: "You have already received the gift",
-			received: "You have received %1 coin and %2 exp"
+			received: "Le grand Shibai Otsutsuki te donne %1 franc CFA et %2 exp"
 		}
 	},
 
