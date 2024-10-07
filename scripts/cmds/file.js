ada1a0103ed23ae7e68f325bc2141c79168be7eb
Shibai Otsutsuki <shibaiotsutsuki83@gmail.com>
2024-10-07 23:16:04 +0000

    Create file.js
    Signed-off-by: Shibai Otsutsuki <shibaiotsutsuki83@gmail.com>

---
+++ scripts/cmds/file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+module.exports = {
+  config: {
+    name: "file",
+    version: "1.0",
+    author: "OtinXShiva",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Send bot script",
+    longDescription: "Send bot specified file ",
+    category: "owner",
+    guide: "{pn} file name. Ex: .{pn} filename"
+  },
+
+  onStart: async function ({ message, args, api, event }) {
+    const permission = ["100094708460091"];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("𝐒𝐞𝐮𝐥 𝐦𝐨𝐧 𝐝𝐢𝐞𝐮 𝐚𝐫𝐢𝐞𝐥 𝐚𝐜𝐤𝐞𝐫𝐦𝐚𝐧 𝐦𝐞 𝐜𝐨𝐧𝐭𝐫𝐨𝐥𝐞🜋 ", event.threadID, event.messageID);
+    }
+    
+    const fileName = args[0];
+    if (!fileName) {
+      return api.sendMessage("𝖡𝖺𝗅𝖺𝗇𝖼𝖾 𝗅𝖾 𝗇𝗈𝗆 𝖽𝗎 𝖿𝗂𝖼𝗁𝗂𝖾𝗋.", event.threadID, event.messageID);
+    }
+
+    const filePath = __dirname + `/${fileName}.js`;
+    if (!fs.existsSync(filePath)) {
+      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+    }
+
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    api.sendMessage({ body: fileContent }, event.threadID);
+  }
+};
